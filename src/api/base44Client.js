e2267db0e30a0cae8966e35390bb8999a8921f1e
Muhammad Aizazullah <muhammadaizazullah456@gmail.com>
2026-07-06 17:26:55 +0500

    Fixed API keys and manifest path

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,14 +1,8 @@
 import { createClient } from '@base44/sdk';
-import { appParams } from '@/lib/app-params';
 
-const { appId, token, functionsVersion, appBaseUrl } = appParams;
-
-//Create a client with authentication required
 export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  serverUrl: '',
-  requiresAuth: false,
-  appBaseUrl
+  appId: "6a4ad5f51ab3c1fe042dfc6a",
+  headers: {
+    "api\u005Fkey": "38e962cbb69d4398a6061ea25b02761b"
+  }
 });
