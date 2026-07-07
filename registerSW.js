b3d85f7492ec2c0f4b424d3b455afa9c17962b53
Muhammad Aizazullah <muhammadaizazullah456@gmail.com>
2026-07-07 13:53:34 +0500

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Inventory-Management-System/sw.js', { scope: '/Inventory-Management-System/' })})}
